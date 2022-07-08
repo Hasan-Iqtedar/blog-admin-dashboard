a9b8e0ca4632abc9c07657ae007473bc1c9b918d
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-07-08 16:00:51 +0500

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+
 const App = (props) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    console.log('App mounting\n_________________');
+    if (!localStorage.token) {
+      navigate('/login', { replace: true });
+    }
+    return () => {
+      console.log('App unmounting\n______________');
+      // localStorage.removeItem('token');
+    };
+  }, []);
+
   return (
     <>
-      <h1>Hello World</h1>
+      <Dashboard />
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './context/auth';
 import App from './App';
+import LoginPage from './components/LoginPage';
+import RequireAuth from './components/RequireAuth';
+
+import './styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <RequireAuth>
+                <App />
+              </RequireAuth>
+            }
+          />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
