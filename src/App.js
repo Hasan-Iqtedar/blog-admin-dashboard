9eb9e54482ae1cc2ca3dabb8701e89934580aa3a
Hasan Iqtedar <hasan.iqtedar@gmail.com>
2022-07-09 20:58:15 +0500

    Update routes and restructure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import CreatePost from './components/CreatePost';
 import NavigationBar from './components/NavigationBar';
+import { URL } from './constants/utils';
 
 const App = (props) => {
+  const [posts, setPosts] = useState([]);
+
+  const updatePosts = (posts) => setPosts(posts);
+
   useEffect(() => {
     console.log('App mounting\n_________________');
+
+    fetch(URL + '/posts', {
+      method: 'GET',
+      headers: { 'Content-Type': 'Application/json' },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        updatePosts(data);
+      });
 
     return () => {
       console.log('App unmounting\n______________');
@@ -17,8 +32,22 @@ const App = (props) => {
     <>
       <NavigationBar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create-post" element={<CreatePost />} />
+        <Route
+          path="/"
+          element={<Home posts={posts} updatePosts={updatePosts} />}
+        />
+        <Route
+          path="/create-post"
+          element={
+            <CreatePost posts={posts} updatePosts={updatePosts} method="POST" />
+          }
+        />
+        <Route
+          path="/update-post/:postId"
+          element={
+            <CreatePost posts={posts} updatePosts={updatePosts} method="PUT" />
+          }
+        />
       </Routes>
     </>
   );
